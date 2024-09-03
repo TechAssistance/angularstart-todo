23aa8909e49d3889bfa9adeb502b5f6b26468707
Nicholas Decker <nicholasdecker@Nicholass-MBP.home>
2024-09-03 00:01:49 -0500

    All the files
    Added small todo. Need to add details page and descriptions to render in
    UI comp.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
-import { TodoFormComponent } from "./ui/todo-form.component";
+import { Component, inject } from '@angular/core';
+import { TodoFormComponent } from './ui/todo-form.component';
+import { TodoService } from '../shared/data-access/todo.service';
+import { TodoListComponent } from './ui/todo-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   template: `
     <h2>Todo</h2>
-    <app-todo-form />
+    <app-todo-form (todoSubmitted)="todoService.addTodo($event)" />
+    <app-todo-list [todos]="todoService.todos()" />
   `,
-  imports: [TodoFormComponent],
+  imports: [TodoFormComponent, TodoListComponent],
 })
 export default class HomeComponent {
-
+  todoService = inject(TodoService);
 }
--- src/app/home/ui/todo-form.component.ts
+++ src/app/home/ui/todo-form.component.ts
@@ -1,24 +1,30 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Todo } from '../../shared/interfaces/todo';
 
 @Component({
   standalone: true,
   selector: 'app-todo-form',
   template: `
-    <form [formGroup]="todoForm">
+    <form
+      [formGroup]="todoForm"
+      (ngSubmit)="todoSubmitted.emit(todoForm.getRawValue())"
+    >
       <input type="text" formControlName="title" placeholder="title..." />
       <input
         type="text"
         formControlName="description"
         placeholder="description..."
       />
-      <button type="submit">Add todo</button>
+      <button [disabled]="!todoForm.valid" type="submit">Add todo</button>
     </form>
   `,
   imports: [ReactiveFormsModule],
 })
 export class TodoFormComponent {
   private fb = inject(FormBuilder);
+
+  todoSubmitted = output<Todo>();
 
   todoForm = this.fb.nonNullable.group({
     title: ['', Validators.required],

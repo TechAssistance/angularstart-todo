6f4f4facdab87e313b09058247ee3aeb188cc3a8
Nicholas Decker <nicholasdecker@Nicholass-MBP.lan>
2024-09-03 12:59:14 -0500

    Add material design and finish details routing page

--- src/app/home/ui/todo-form.component.ts
+++ src/app/home/ui/todo-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, inject, output } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CreateTodo, Todo } from '../../shared/interfaces/todo';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   standalone: true,
@@ -9,17 +11,32 @@ import { CreateTodo, Todo } from '../../shared/interfaces/todo';
     <form
       [formGroup]="todoForm"
       (ngSubmit)="todoSubmitted.emit(todoForm.getRawValue())"
+      class="example-form"
     >
-      <input type="text" formControlName="title" placeholder="title..." />
-      <input
-        type="text"
-        formControlName="description"
-        placeholder="description..."
-      />
-      <button [disabled]="!todoForm.valid" type="submit">Add todo</button>
+      <mat-form-field class="example-full-width">
+        <mat-label>Title</mat-label>
+        <input
+          matInput
+          type="text"
+          formControlName="title"
+          placeholder="Ex: Do the dishes"
+        />
+      </mat-form-field>
+      <mat-form-field class="example-full-width">
+        <mat-label>Description</mat-label>
+        <input
+          matInput
+          type="text"
+          formControlName="description"
+          placeholder="description..."
+        />
+      </mat-form-field>
+      <button mat-raised-button [disabled]="!todoForm.valid" type="submit">
+        Add todo
+      </button>
     </form>
   `,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatInputModule, MatButtonModule],
 })
 export class TodoFormComponent {
   private fb = inject(FormBuilder);
--- src/app/home/ui/todo-list.component.ts
+++ src/app/home/ui/todo-list.component.ts
@@ -1,22 +1,23 @@
 import { Component, input } from '@angular/core';
 import { Todo } from '../../shared/interfaces/todo';
 import { RouterLink } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   standalone: true,
   selector: 'app-todo-list',
   template: `
-    <ul>
+    <mat-list role="list">
       @for (todo of todos(); track todo.id) {
-        <li>
-          <a routerLink="/detail/{{ todo.id }}">{{ todo.title }}</a>
-        </li>
+        <mat-list-item>
+          <span routerLink="/detail/{{ todo.id }}">{{ todo.title }}</span>
+        </mat-list-item>
       } @empty {
-        <li>Nothing to do!</li>
+        <mat-list-item>Nothing Todo!</mat-list-item>
       }
-    </ul>
+    </mat-list>
   `,
-  imports: [RouterLink],
+  imports: [RouterLink, MatListModule],
 })
 export class TodoListComponent {
   todos = input.required<Todo[]>();

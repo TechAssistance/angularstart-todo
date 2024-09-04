e8fe9b90fa50ef4b711030f041cd8804f7f69f43
TechAssistance <76240748+TechAssistance@users.noreply.github.com>
2024-09-03 23:56:19 -0500

    test again to fix commit info

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-// test update for git lol
+// test update for git lol - okay think this should fix the commit info lol
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
 );
